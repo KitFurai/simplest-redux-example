a0f6094fa213922d0983db29125dcc6bd0496a50
Katerina <katerina@dwp.co.id>
2018-09-28 15:46:05 +0700

    Add folder src

---
+++ src/newpage.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider, connect } from 'react-redux'
+
+// React component
+class Counter2 extends Component {
+  render() {
+    const { value, onIncreaseClick } = this.props
+    return (
+      <div>
+        <span>{value}</span>
+        <button onClick={onIncreaseClick}>Increase</button>
+      </div>
+    )
+  }
+}
